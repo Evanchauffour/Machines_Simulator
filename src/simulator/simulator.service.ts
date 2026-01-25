1ee53e5cb40201cb56047689652479d568991957
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2026-01-25 17:53:52 +0100

    Remove unused machines for this course

--- src/simulator/simulator.service.ts
+++ src/simulator/simulator.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Socket, io } from 'socket.io-client';
 import { Convoyeur } from './types/convoyeur';
-import { PresseHydraulique } from './types/press';
 import { Imprimante3D } from './types/imprimante3d';
-import { CompresseurFrigorifique } from './types/compresseur-frigorifique';
 
 @Injectable()
 export class SimulatorService {
   private readonly socket: Socket;
   private readonly machineId = process.env.MACHINE_ID || 'machine-default';
   private readonly machineType = process.env.MACHINE_TYPE || 'convoyeur';
-  private instance:
-    | Convoyeur
-    | PresseHydraulique
-    | Imprimante3D
-    | CompresseurFrigorifique;
+  private instance: Convoyeur | Imprimante3D;
 
   constructor() {
     this.socket = io(process.env.BACKEND_URL || '', {
@@ -34,26 +28,13 @@ export class SimulatorService {
         case 'convoyeur-bande':
           this.instance = new Convoyeur(this.machineId, this.socket);
           break;
-        case 'presse':
-        case 'presse-hydraulique':
-          this.instance = new PresseHydraulique(this.machineId, this.socket);
-          break;
         case 'imprimante3d':
         case 'imprimante-3d':
           this.instance = new Imprimante3D(this.machineId, this.socket);
           break;
-        case 'compresseur':
-        case 'compresseur-frigorifique':
-          this.instance = new CompresseurFrigorifique(
-            this.machineId,
-            this.socket,
-          );
-          break;
         default:
           console.error(`Type de machine inconnu : ${this.machineType}`);
-          console.log(
-            'Types disponibles: convoyeur, presse-hydraulique, imprimante3d, compresseur-frigorifique',
-          );
+          console.log('Types disponibles: convoyeur, imprimante3d');
           process.exit(1);
       }
 
